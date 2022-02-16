4de9cfa7ecd3fe7bab16293d1470e59091e49de8
Richie <ricardo.andres.guerra.santana@gmail.com>
2022-02-16 15:38:32 +1000

    🔧 eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/*global module*/
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    }
+}
